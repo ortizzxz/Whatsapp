01a74a16af86d3301d716c52653ea260a526483c
Jesus Ortiz <jesusdortiz18@gmail.com>
2025-02-25 20:36:26 +0100

    upload socker url

--- client/whatsappClone/src/main.js
+++ client/whatsappClone/src/main.js
@@ -2,7 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import { io } from "socket.io-client";
 import './assets/styles.css'
-const socket = io("http://localhost:3000"); 
+const socket = io("https://whatsapp-mmgt.onrender.com"); 
 
 const app = createApp(App);
 
